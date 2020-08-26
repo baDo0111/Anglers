5bde209cff446f1e083353afcba2dcace4a4225b
vagrant <vagrant@localhost.localdomain>
2020-08-26 16:47:13 +0000

    logo home/index complete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,13 +20,13 @@ jQuery(document).on('turbolinks:load', function(){
     $(document).ready(function () {
       $("#theTarget").skippr({
         // スライドショーの変化 ("fade" or "slide")
-        transition : 'slide',
+        transition : 'fade',
         // 変化に係る時間(ミリ秒)
         speed : 1000,
         // easingの種類
         easing : 'easeOutQuart',
         // ナビゲーションの形("block" or "bubble")
-        navType : 'block',
+        navType : 'bubble',
         // 子要素の種類("div" or "img")
         childrenElementType : 'div',
         // ナビゲーション矢印の表示(trueで表示)
@@ -36,7 +36,7 @@ jQuery(document).on('turbolinks:load', function(){
         // 自動再生時のスライド切替間隔(ミリ秒)
         autoPlayDuration : 3000,
         // キーボードの矢印キーによるスライド送りの設定(trueで有効)
-        keyboardOnAlways : true,
+        keyboardOnAlways : false,
         // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
         hidePrevious : false
       });
