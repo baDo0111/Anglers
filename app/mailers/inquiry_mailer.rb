9200bd9fce996e886ebf7ee69f73fa16bf75f187
vagrant <vagrant@localhost.localdomain>
2020-11-26 15:33:43 +0000

    [fix]

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -2,8 +2,8 @@ class InquiryMailer < ApplicationMailer
 	def send_mail(inquiry)
 	    @inquiry = inquiry
 	    mail(
-	    	from: 'sakanatarou891@gmail.com'
-	      	to:   ENV['MAIL_TO'],
+	    	from: 'sakanatarou891@gmail.com',
+	      	to:   'sakanatarou891@gmail.com',
 	      	subject: 'お問い合わせ通知'
 	    )
 	end
